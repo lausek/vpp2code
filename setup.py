0775ff930696add4b00d9d4b89413969bd347e68
lausek <root@lausek.eu>
2020-11-30 14:46:33 +0100

    improve installer (still not working properly)

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='vpp2code',
     author='lausek',
     version='0.0.1',
+    description='generate java code from vpp files',
+    url='https://github.com/lausek/vpp2code',
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'lark'
     ],
